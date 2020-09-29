c8baad679802698e922d2e511620a654af525e00
luis-padilla-tech <luispadilla.tech>
2020-09-28 22:29:30 -0700

    Repeating timer

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,26 @@
 document.getElementById('btn_startTimer').addEventListener('click', ()=>{
-    startTimer(defaultStartTimeSeconds);
+    isOnBreak = false;
+    startTimer(WORK_TIME);
 })
 
 document.getElementById('btn_stopTimer').addEventListener('click', ()=>{
     stopTimer();
 })
 
-const defaultStartTimeSeconds = 4;
-const defaultshortBreaksBeforeLongBreakSeconds = 3;
-const defaultShortBreakStartTimeSeconds = 2;
-const defaultLongBreaksStartTimeSeconds = 5;
-var shortBreaksTaken = 0;
+const WORK_TIME = 6;
+const SHORT_BREAK_TIME = 2;
+const LONG_BREAK_TIME = 10;
+const SHORT_BREAKS_BEFORE_LONG_BREAK = 4;
+var breaksTaken = 0;
+
+var isOnBreak = false;
 var timeLeftSeconds;
 var timerInterval;
 
 function startTimer(seconds){
     timerInterval = setInterval(timerTick, 1000);
     timeLeftSeconds = seconds;
+    changeTimeText()
 }
 
 function stopTimer(){
@@ -24,29 +28,53 @@ function stopTimer(){
 }
 
 function timerTick(){
-    let minutes = '' + Math.floor(timeLeftSeconds / 60);
-    let seconds = '' + timeLeftSeconds % 60;
-
-    let minutesText = minutes.padStart(2 ,'0')
-    let secondsText = seconds.padStart(2, '0');
 
     timeLeftSeconds--;
 
+    changeTimeText();
+
     if(timeLeftSeconds < 0) {
         stopTimer();
-        takeBreak();
+        displayNotification();
     }
-
-    let newTimerText = `${minutesText}:${secondsText}`;
     
+}
+
+function changeTimeText() {
+    let minutes = '' + Math.floor(timeLeftSeconds / 60);
+    let seconds = '' + timeLeftSeconds % 60;
+    
+    let minutesText = minutes.padStart(2 ,'0')
+    let secondsText = seconds.padStart(2, '0');
+    
+    let newTimerText = `${minutesText}:${secondsText}`;
     document.getElementById('timerText').textContent = newTimerText
 }
 
-function takeBreak(){
+function displayNotification(){
 
-    shortBreaksTaken++;
+    const notice = new Notification( 'Tiki - Stay Strong', {
 
-    const notice = new Notification( 'You are done',{
-        body: 'Great Work Take a break'
+        body: getNotificationBody()
     });
+
+    createNewTimer();
+}
+
+function getNotificationBody() {
+    return isOnBreak  ? 'Let start the grind again' : 'Break time';
+}
+
+function createNewTimer() {
+    if (isOnBreak){
+        startTimer(WORK_TIME)
+    }else if (breaksTaken >= SHORT_BREAKS_BEFORE_LONG_BREAK){
+        breaksTaken = 0;
+        startTimer(LONG_BREAK_TIME);
+    }else{
+        breaksTaken++;
+        startTimer(SHORT_BREAK_TIME);
+    }
+
+    isOnBreak = !isOnBreak;
 }
